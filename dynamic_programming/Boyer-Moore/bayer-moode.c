8a57f0dafd984df0983f0a7eac0b11ebace2c783
agayev169 <agayev169@gmail.com>
2018-10-22 20:17:35 +0400

    Add Boyer-Moore Algorithm

---
+++ dynamic_programming/Boyer-Moore/bayer-moode.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Declarations
+int find_in_word(char *to_find, char *word);
+
+
+// Definitions
+int find_in_word(char *to_find, char *word) {
+    char *shift = (char*) calloc(len(to_find), sizeof(char));
+}
---
+++ dynamic_programming/Boyer-Moore/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <bayer-moode.c>
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
