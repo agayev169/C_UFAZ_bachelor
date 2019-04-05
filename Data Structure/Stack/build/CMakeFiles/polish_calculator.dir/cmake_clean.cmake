6a3b1062c0824c80d12cf8f124ab17d069bbc959
agayev169 <agayev169@gmail.com>
2019-04-05 12:12:09 +0400

    Add CUnit testing

---
+++ Data Structure/Stack/build/CMakeFiles/polish_calculator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/polish_calculator.dir/main.c.o"
+  "CMakeFiles/polish_calculator.dir/lifo.c.o"
+  "../bin/polish_calculator.pdb"
+  "../bin/polish_calculator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/polish_calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
