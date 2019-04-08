fd44ef7d72a8e86e42080822e86caa9ac9c7b129
agayev169 <agayev169@gmail.com>
2019-04-08 23:26:16 +0400

    Still fixing travis

--- Data Structure/Stack/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/agayev169/C_UFAZ_bachelor/Data Structure/Stack
-# Build directory: /home/agayev169/C_UFAZ_bachelor/Data Structure/Stack/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test1 "testing")
